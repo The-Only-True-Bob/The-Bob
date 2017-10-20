f47823d822e42d99d3a18ff0815bc43b2302d71a
Andrei Tcibin <andrei_tcibin@epam.com>
2017-10-20 19:23:15 +0300

    feat: Update Main server class

--- src/main/java/com/github/the_only_true_bob/the_bob/Main.java
+++ src/main/java/com/github/the_only_true_bob/the_bob/Main.java
@@ -1,43 +1,31 @@
 package com.github.the_only_true_bob.the_bob;
 
 import com.github.the_only_true_bob.the_bob.handler.Handler;
-import com.github.the_only_true_bob.the_bob.handler.Message;
+import com.github.the_only_true_bob.the_bob.vk.Message;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.vk.api.sdk.client.TransportClient;
-import com.vk.api.sdk.client.VkApiClient;
-import com.vk.api.sdk.client.actors.GroupActor;
-import com.vk.api.sdk.httpclient.HttpTransportClient;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class Main {
 
     @Autowired
     private static Handler bobHandler;
+    @Value("${confirmation.code}")
+    private static String confirmationCode;
 
     public static void main(String[] args) throws Exception {
-        final TransportClient transportClient = HttpTransportClient.getInstance();
-        final VkApiClient api = new VkApiClient(transportClient);
-
         final InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("config.properties");
-        final Properties properties = new Properties();
-        properties.load(inputStream);
-
-        final int groupId = Integer.parseInt(properties.getProperty("groupId"));
-        final String token = properties.getProperty("token");
-        final String confirmationCode = properties.getProperty("confirmationCode");
-        final GroupActor actor = new GroupActor(groupId, token);
         //port from heroku server
         final int port = Integer.parseInt(System.getenv("PORT"));
 
