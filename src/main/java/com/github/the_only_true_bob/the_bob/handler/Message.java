2287552f78f1be16f7ba4e3ebbf4b86f77cb3efe
Andrei Tcibin <andrei_tcibin@epam.com>
2017-10-20 17:53:42 +0300

    feat: Add Message::from

--- src/main/java/com/github/the_only_true_bob/the_bob/handler/Message.java
+++ src/main/java/com/github/the_only_true_bob/the_bob/handler/Message.java
@@ -1,5 +1,12 @@
 package com.github.the_only_true_bob.the_bob.handler;
 
+import com.google.gson.JsonObject;
+
 public interface Message {
     // TODO: 20.10.17 Figure out how it should work and what it must consist
+
+    static Message from(final JsonObject body) {
+        // TODO: 20/10/17 parseJson
+        return null;
+    }
 }
