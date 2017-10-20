3a6692098661314a0f5c41683bcf888d02d9dcaa
Denis Verkhoturov <denis_verkhoturov@epam.com>
2017-10-20 17:23:41 +0300

    feat: Boilerplate for Handler and message entities

---
+++ src/main/java/com/github/the_only_true_bob/the_bob/handler/Handler.java
@@ -0,0 +1,9 @@
+package com.github.the_only_true_bob.the_bob.handler;
+
+import java.util.function.Consumer;
+
+public interface Handler extends Consumer<Message> {
+    default void accept(final Message message) {
+        // TODO: 20.10.17 Accept message and process
+    }
+}
---
+++ src/main/java/com/github/the_only_true_bob/the_bob/handler/Message.java
@@ -0,0 +1,5 @@
+package com.github.the_only_true_bob.the_bob.handler;
+
+public interface Message {
+    // TODO: 20.10.17 Figure out how it should work and what it must consist
+}
