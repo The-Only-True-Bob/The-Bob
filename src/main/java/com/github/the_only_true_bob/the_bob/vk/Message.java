23fdb8a430e33f959a1d2548b37b3f753d3541e9
Denis Verkhoturov <denis_verkhoturov@epam.com>
2017-10-20 18:42:33 +0300

    feat: Add contract to Message (type, text, attachments, userId)

--- src/main/java/com/github/the_only_true_bob/the_bob/vk/Message.java
+++ src/main/java/com/github/the_only_true_bob/the_bob/vk/Message.java
@@ -5,6 +5,15 @@ import com.google.gson.JsonObject;
 public interface Message {
     // TODO: 20.10.17 Figure out how it should work and what it must consist
 
+    Type type();
+
+    String text();
+
+    // TODO: 20.10.17 It must somewhat
+    void attachments();
+
+    String userId();
+
     static Message from(final JsonObject body) {
         return null;
     }
---
+++ src/main/java/com/github/the_only_true_bob/the_bob/vk/Type.java
@@ -0,0 +1,5 @@
+package com.github.the_only_true_bob.the_bob.vk;
+
+public enum Type {
+    POLL, MESSAGE;
+}
