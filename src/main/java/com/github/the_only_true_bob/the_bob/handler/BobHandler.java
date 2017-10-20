89b525c03f4b21c2c6cf5eea886630fb8ce3899e
Denis Verkhoturov <denis_verkhoturov@epam.com>
2017-10-21 00:27:26 +0300

    fix: Change message type processing for type message_new

--- src/main/java/com/github/the_only_true_bob/the_bob/handler/BobHandler.java
+++ src/main/java/com/github/the_only_true_bob/the_bob/handler/BobHandler.java
@@ -5,16 +5,19 @@ import com.github.the_only_true_bob.the_bob.vk.Message;
 class BobHandler implements Handler {
     @Override
     public void accept(Message message) {
-        // TODO: 20.10.17 Accept message and process
         System.err.println("That's the way the cookie crumbles!");
         switch (message.type()) {
             case MESSAGE:
                 // TODO: 20.10.17 Figure out what does he talking about and does his message have an attachments
+                System.out.println("processing message");
                 break;
             case POLL:
                 // TODO: 20.10.17 Update user preferences in database
+                System.out.println("processing poll");
+                break;
             default:
                 // TODO: 20.10.17 Send response message to the conversation
+                System.out.println("processing unknown message");
         }
     }
 }
--- src/main/java/com/github/the_only_true_bob/the_bob/vk/MessageType.java
+++ src/main/java/com/github/the_only_true_bob/the_bob/vk/MessageType.java
@@ -1,12 +1,11 @@
 package com.github.the_only_true_bob.the_bob.vk;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 import static java.util.stream.Collectors.toList;
@@ -17,30 +16,45 @@ public enum MessageType {
         public Optional<Message> parse(JsonObject body) {
             final JsonObject objectJson = body.get("object").getAsJsonObject();
             return Optional.ofNullable(
-                    getBuilder(this, objectJson)
+                    Message.builder()
+                            .setType(this)
+                            .setUserVkId(objectJson.get("user_id").getAsString())
                             .setPollId(objectJson.get("poll_id").getAsString())
                             .setOptionId(objectJson.get("option_id").getAsString())
-                            .build());
+                            .build()
+            );
         }
     },
 
     MESSAGE("message_new") {
         @Override
         public Optional<Message> parse(JsonObject body) {
-            final JsonObject objectJson = body.get("object").getAsJsonObject();
-            final JsonArray attachmentsJson = objectJson.get("attachments").getAsJsonArray();
-            final List<Attachment> attachments = StreamSupport.stream(attachmentsJson.spliterator(), false)
+            Message.Builder builder = Message.builder().setType(this);
+            Optional.ofNullable(body.get("object"))
                     .map(JsonElement::getAsJsonObject)
-                    .flatMap(attachment -> {
-                        final String type = attachment.get("type").getAsString();
-                        return AttachmentType.of(type).parse(attachment).orElseGet(Collections::emptyList).stream();
+                    .map(object -> {
+                        final String user_id = Optional.ofNullable(object.get("user_id"))
+                                                       .map(JsonElement::getAsString)
+                                                       .orElse("");
+                        final String text = Optional.ofNullable(object.get("body"))
+                                                    .map(JsonElement::getAsString)
+                                                    .orElse("");
+                        builder.setUserVkId(user_id)
+                               .setText(text);
+                        return object.get("attachments");
                     })
-                    .collect(toList());
-            return Optional.ofNullable(
-                    getBuilder(this, objectJson)
-                            .setText(objectJson.get("body").getAsString())
-                            .setAttachments(attachments)
-                            .build());
+                    .map(attachments -> StreamSupport.stream(attachments.getAsJsonArray().spliterator(), false)
+                                    .map(JsonElement::getAsJsonObject)
+                                    .flatMap(attachment -> Optional.ofNullable(attachment.get("type"))
+                                            .map(JsonElement::getAsString)
+                                            .flatMap(type -> AttachmentType.of(type).parse(attachment).map(List::stream))
+                                            .orElseGet(Stream::empty)
+                                    )
+                                    .collect(toList())
+                    )
+                    .ifPresent(builder::setAttachments);
+
+            return Optional.ofNullable(Message.builder().build());
         }
     },
 
@@ -60,12 +74,6 @@ public enum MessageType {
     @Override
     public String toString() {
         return string;
-    }
-
-    private static Message.Builder getBuilder(final MessageType type, final JsonObject objectJson) {
-        return Message.builder()
-                .setType(type)
-                .setUserVkId(objectJson.get("user_id").getAsString());
     }
 
     public static MessageType of(String type) {
