669ddc341d2b0e08abe2d92b7e8923052ef92428
Denis Verkhoturov <denis_verkhoturov@epam.com>
2017-10-20 22:43:30 +0300

    feat: Add clearifying comments to Handler::accept logic

--- src/main/java/com/github/the_only_true_bob/the_bob/handler/Handler.java
+++ src/main/java/com/github/the_only_true_bob/the_bob/handler/Handler.java
@@ -7,6 +7,17 @@ import java.util.function.Consumer;
 public interface Handler extends Consumer<Message> {
     default void accept(final Message message) {
         // TODO: 20.10.17 Accept message and process
+        System.err.println("That's the way the cookie crumbles!");
+        switch (message.type()) {
+            case MESSAGE:
+                // TODO: 20.10.17 Figure out what does he talking about and does his message have an attachments
+                break;
+            case POLL:
+                // TODO: 20.10.17 Update user preferences in database
+            default:
+                // TODO: 20.10.17 Send response message to the conversation
+
+        }
     }
 
     static Handler bob() {
