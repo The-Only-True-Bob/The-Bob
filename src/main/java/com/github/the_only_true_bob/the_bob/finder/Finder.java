41431e5cbcf3a2384b001566d7cfc4abcb37104a
LittleBelka <kompp@yandex.ru>
2017-10-22 01:14:59 +0300

    feat: Add finder implementation for audio

--- src/main/java/com/github/the_only_true_bob/the_bob/finder/Finder.java
+++ src/main/java/com/github/the_only_true_bob/the_bob/finder/Finder.java
@@ -3,8 +3,8 @@ package com.github.the_only_true_bob.the_bob.finder;
 import com.github.the_only_true_bob.the_bob.dao.entitites.EventEntity;
 import com.github.the_only_true_bob.the_bob.vk.Message;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 public interface Finder {
-    List<EventEntity> suggestEventsFor(Message message);
+    Stream<EventEntity> suggestEventsFor(Message message);
 }
---
+++ src/main/java/com/github/the_only_true_bob/the_bob/finder/FinderImpl.java
@@ -0,0 +1,45 @@
+package com.github.the_only_true_bob.the_bob.finder;
+
+import com.github.the_only_true_bob.the_bob.dao.DataService;
+import com.github.the_only_true_bob.the_bob.dao.entitites.EventEntity;
+import com.github.the_only_true_bob.the_bob.vk.Attachment;
+import com.github.the_only_true_bob.the_bob.vk.AttachmentType;
+import com.github.the_only_true_bob.the_bob.vk.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.stream.Stream;
+
+class FinderImpl implements Finder {
+
+    @Autowired
+    private DataService dataService;
+
+    @Override
+    public Stream<EventEntity> suggestEventsFor(final Message message) {
+        final Stream<String> attachmentBasedSuggestion = message.attachments()
+                .stream()
+                .filter(this::isAudio)
+                .map(audio -> audio.audioArtist().get());
+
+        final Stream<String> textBasedSuggestion = message.text()
+                .map(Stream::of)
+                .orElseGet(Stream::empty);
+
+        return Stream
+                .concat(attachmentBasedSuggestion, textBasedSuggestion)
+                .flatMap(this::suggest);
+    }
+
+    private boolean isAudio(Attachment attachment) {
+        return AttachmentType.AUDIO.equals(attachment.type())
+                && attachment.audioArtist().isPresent();
+    }
+
+    private Stream<EventEntity> suggest(final String needle) {
+        final String textMessage = needle.toLowerCase();
+        return dataService
+                .findAllEventEntities()
+                .stream()
+                .filter(event -> textMessage.contains(event.getName().toLowerCase()));
+    }
+}
