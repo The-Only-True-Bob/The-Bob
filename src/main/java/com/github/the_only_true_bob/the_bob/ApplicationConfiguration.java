6c1d4895bd1939b75a2bbf2c436ccb151a468896
Denis Verkhoturov <denis_verkhoturov@epam.com>
2017-10-21 19:34:04 +0300

    chore: Migrate to mysql

--- src/main/java/com/github/the_only_true_bob/the_bob/ApplicationConfiguration.java
+++ src/main/java/com/github/the_only_true_bob/the_bob/ApplicationConfiguration.java
@@ -23,12 +23,10 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.persistence.EntityManagerFactory;
@@ -44,6 +42,15 @@ import static java.util.stream.Collectors.toList;
 @EnableJpaRepositories("com.github.the_only_true_bob.the_bob")
 @PropertySource("classpath:config.properties")
 public class ApplicationConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String databaseUrl;
+
+    @Value("${spring.datasource.username}")
+    private String databaseUser;
+
+    @Value("${spring.datasource.password}")
+    private String databasePassword;
 
     @Value("${confirmation_code}")
     private String confirmationCode;
@@ -135,15 +142,17 @@ public class ApplicationConfiguration {
 
     @Bean
     public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(databaseUrl);
+        dataSource.setUsername(databaseUser);
+        dataSource.setPassword(databasePassword);
+        return dataSource;
     }
 
     @Bean
     public JpaVendorAdapter jpaVendorAdapter() {
-        HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
-        bean.setDatabase(Database.H2);
-        bean.setGenerateDdl(true);
-        return bean;
+        return new HibernateJpaVendorAdapter();
     }
 
     @Bean
